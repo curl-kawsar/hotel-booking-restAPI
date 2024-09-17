70d4c3ed70a9a6849e07663205d13c0f81301da8
Md. Kawsar Ahmed <knownaskawsar@gmail.com>
2024-09-17 22:42:18 +0600

    Update settings.py

--- journey/settings.py
+++ journey/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -23,9 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--swidmy$k)&ut8u#hqa$#3q9k-ct#lxa3*378fs&wx=8$ziz4$'
 DEBUG = True
 # SECURITY WARNING: don't run with debug turned on in production!
-ALLOWED_HOSTS = ["https://travel-junction.onrender.com"]
-CSRF_TRUSTED_ORIGINS = ['https://travel-junction.onrender.com', 'https://*127.0.0.1']
+ALLOWED_HOSTS = ["travel-junction.onrender.com"]
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://travel-junction.onrender.com',
+    'http://127.0.0.1',
+    'https://127.0.0.1'
+]
 
 # Application definition
 
@@ -70,7 +73,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'journey.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -80,7 +82,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -100,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -111,7 +111,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
